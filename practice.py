4caac0e911ee5d07dcb85817117ae5fb9de40f08
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-15 22:59:32 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,20 @@
-print('hello')
-print('world')
-print('world')
+#List Comprehensions
+def capitalize_all(t)
+    res = []
+    for s in t:
+      res.append(s.capitalize())
+    
+    return res
+  
+  
+def capitalize_all(t):
+    return[s.capitalize() for s in t]
+  
+
+def only_upper(t):
+    res = []
+    for s in t:
+      if s.isupper():
+         res.append(s)
+      
+      return res
